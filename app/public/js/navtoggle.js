ed47ebff1a95ac45dcf770607da584370c777bc0
IAmBatJew <aamigarr@gmail.com>
2025-01-26 16:37:08 -0500

    Setting to match the published site

--- app/public/js/navtoggle.js
+++ app/public/js/navtoggle.js
@@ -1,10 +1,11 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.navbar-toggle');
     const menu = document.querySelector('.navbar-menu');
 
-    toggleButton.addEventListener('click', function () {
+    function toggleMenu() {
         menu.classList.toggle('show');
-    });
+    }
+
+    toggleButton.addEventListener('click', toggleMenu);
+    toggleButton.addEventListener('touchend', toggleMenu);
 });
